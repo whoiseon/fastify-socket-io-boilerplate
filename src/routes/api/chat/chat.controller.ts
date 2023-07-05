c2cb9289e9017733ed5c719cc7ac9c3502b382ad
whoiseon <whois__@naver.com>
2023-07-05 17:08:31 +0900

    feat: change the body of the room create api

--- src/routes/api/chat/chat.controller.ts
+++ src/routes/api/chat/chat.controller.ts
@@ -3,15 +3,18 @@ import plugins from '../../../plugins';
 import { AppRequest } from '../../../lib/types';
 import ChatService from './chat.service';
 
-export default class ChatController {
+export default class ChatController
+{
   private chatService: ChatService = new ChatService();
 
-  constructor() {}
+  constructor() { }
 
-  public privateRoutes: FastifyPluginAsync = async (fastify) => {
+  public privateRoutes: FastifyPluginAsync = async (fastify) =>
+  {
     fastify.register(plugins.requireAuthPlugin);
 
-    fastify.post('/private/message', async (request: AppRequest) => {
+    fastify.post('/private/message', async (request: AppRequest) =>
+    {
       const body = JSON.parse(request.body as string);
       const { id: userId } = request.user!;
       const { roomCode, content } = body;
@@ -25,10 +28,11 @@ export default class ChatController {
       });
     });
 
-    fastify.post('/private/create', async (request: AppRequest) => {
+    fastify.post('/private/create', async (request: AppRequest) =>
+    {
       const { id } = request.user!;
-
-      const { name, description } = request.body as {
+      const body = JSON.parse(request.body as string);
+      const { name, description } = body as {
         name: string;
         description: string;
       };
@@ -40,9 +44,10 @@ export default class ChatController {
       });
     });
 
-    fastify.delete('/private/delete/:id', async (request: AppRequest) => {
+    fastify.delete('/private/delete/:id', async (request: AppRequest) =>
+    {
       const { id: userId } = request.user!;
-      const { id: roomId } = request.params as { id: string };
+      const { id: roomId } = request.params as { id: string; };
 
       return this.chatService.deleteRoom({
         userId,
@@ -51,20 +56,24 @@ export default class ChatController {
     });
   };
 
-  public publicRoutes: FastifyPluginAsync = async (fastify) => {
-    fastify.get('/public/message', async (request: AppRequest) => {
-      const { roomCode } = request.query as { roomCode: string };
+  public publicRoutes: FastifyPluginAsync = async (fastify) =>
+  {
+    fastify.get('/public/message', async (request: AppRequest) =>
+    {
+      const { roomCode } = request.query as { roomCode: string; };
 
       return this.chatService.getMessages(roomCode);
     });
 
-    fastify.get('/public/room/:code', async (request: AppRequest) => {
-      const { code: roomCode } = request.params as { code: string };
+    fastify.get('/public/room/:code', async (request: AppRequest) =>
+    {
+      const { code: roomCode } = request.params as { code: string; };
 
       return this.chatService.getRoomUnique(roomCode);
     });
 
-    fastify.get('/public', async (reqest: AppRequest) => {
+    fastify.get('/public', async (reqest: AppRequest) =>
+    {
       return this.chatService.getRooms();
     });
   };
